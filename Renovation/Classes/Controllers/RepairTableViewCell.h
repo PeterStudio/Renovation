b832bbf8e3d4dde9d189b297cac4e9cb70da45d8
peter <peterstudio@icloud.com>
2015-07-28 17:31:09 +0800

    add RepairTableViewCell & images

---
+++ Renovation/Classes/Controllers/RepairTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  RepairTableViewCell.h
+//  Renovation
+//
+//  Created by Baby on 15/7/28.
+//  Copyright (c) 2015年 peterstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RepairTableViewCell : UITableViewCell
+
+@end
---
+++ Renovation/Classes/Controllers/RepairTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  RepairTableViewCell.m
+//  Renovation
+//
+//  Created by Baby on 15/7/28.
+//  Copyright (c) 2015年 peterstudio. All rights reserved.
+//
+
+#import "RepairTableViewCell.h"
+
+@implementation RepairTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
